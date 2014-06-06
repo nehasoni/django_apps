419ce2e8c9787c8901548435539def0fa621feee
nehasoni <neha.soni.ece10@itbhu.ac.in>
2014-06-06 16:18:42 +0530

    app modified

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -1,5 +1,5 @@
 # Django settings for mysite project.
-
+import os
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/neha/sqlite3.db',                      # Or path to database file if using sqlite3.
+        'NAME': os.path.abspath('sqlite3.db'),                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -107,7 +107,7 @@ ROOT_URLCONF = 'mysite.urls'
 # Python dotted path to the WSGI application used by Django's runserver.
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
-TEMPLATE_DIRS = ('/home/neha/django_apps/templates'
+TEMPLATE_DIRS = (os.path.abspath("django_apps/templates")
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
